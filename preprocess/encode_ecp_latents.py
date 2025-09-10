01d22ba1c4a7ffeeca8490be2889bed32ac364a9
cgturhan <cerenguzell@gmail.com>
2025-09-10 07:35:00 +0200

    Update encode_ecp_latents.py

--- preprocess/encode_ecp_latents.py
+++ preprocess/encode_ecp_latents.py
@@ -63,7 +63,7 @@ def main():
     parser.add_argument("--batch_size", type=int, default=4)
     parser.add_argument("--resolution", type=int, default=1024)
     parser.add_argument("--num_workers", type=int, default=64)
-    parser.add_argument("is_qwen_vae", type = bool, default = False) 
+    parser.add_argument("--is_qwen_vae", type = bool, default = False) 
     args = parser.parse_args()
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
